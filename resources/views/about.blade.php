791e4eac66f1394e65b73571273c4c17cd7f79d0
Kate1199 <kate.kisel99@yandex.by>
2022-04-17 22:22:02 +0300

    add layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,18 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    @extends('layouts.app')
+
+    @section('title', 'О блоге')
+
+    @section('content')
+        <h1>О блоге</h1>
+        <p>Эксперименты с Laravel на Хекслете</p>
+    @endsection
+</body>
+</html>
