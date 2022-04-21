ac502b1b295f22acffb7b65f2a24a67f2f5a33b4
Kate1199 <kate.kisel99@yandex.by>
2022-04-21 14:16:57 +0300

    add show article

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,8 @@ Route::get('/', function () {
 
 Route::get('about', [PageController::class, 'about']);
 Route::get('team', [PageController::class, 'team']);
+
 Route::get('articles', [ArticleController::class, 'index'])
     ->name('articles.index');
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+    ->name('articles.show');
