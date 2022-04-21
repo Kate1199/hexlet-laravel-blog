dc6f809ac9d5a8b6499c47e4e549a232f182755d
Kate1199 <kate.kisel99@yandex.by>
2022-04-21 15:03:18 +0300

    add validation errors output

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+@if ($errors->any())
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 {{ Form::model($article, ['route' => 'articles.store']) }}
     {{ Form::label('name', 'Название') }}
     {{ Form::text('name') }}<br>
